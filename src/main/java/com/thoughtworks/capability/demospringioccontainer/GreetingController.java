11b680a1e6daafc2c9b2307b646426746450da7f
chutting <tingting.chu@thoughtworks.com>
2020-11-07 15:45:05 +0800

    [Chu]: method2: proxyMode

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,23 +1,26 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 
 @RestController
 @Scope(SCOPE_SINGLETON)
 public class GreetingController {
-    @Lookup
-    public GreetingService getGreetingService() {
-        return null;
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return getGreetingService().sayHi();
+        return greetingService.sayHi();
     }
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,13 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @Service
-@Scope(SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+    proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
     public GreetingService() {
         System.out.println("hello world");
